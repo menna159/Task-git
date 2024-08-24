717eb8a03a72a0be511acbb2333439c6f60b4fda
Fadi-Maher <cleverfady@gmail.com>
2024-08-24 21:50:41 +0300

    The product component is added

---
+++ src/components/product.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Product() {
+  return (
+    <div>
+      this is product page created by Fadyyyyyyyyyyyyyyyyyyyyyy
+    </div>
+  )
+}
+
+export default Product
